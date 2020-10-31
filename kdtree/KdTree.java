b91ef0e3cd24917862dcac84dafc1da2a5a7c42c
Blend Hamiti <b.hamiti.9@gmail.com>
2020-10-31 23:29:12 +0100

    started on kd-tree

--- kdtree/KdTree.java
+++ kdtree/KdTree.java
@@ -10,29 +10,30 @@ import edu.princeton.cs.algs4.RectHV;
 import edu.princeton.cs.algs4.SET;
 import edu.princeton.cs.algs4.StdDraw;
 
-// TODO: Change comments to refer to the KdTree
 public class KdTree {
-    private SET<Node> set;
+    private Node root;
 
     // construct an empty set of points
     public KdTree() {
-        set = new SET<>();
+        root = null;
     }
 
     private class Node implements Comparable<Node> {
-        private static final boolean VERTICAL = false;
-        private static final boolean HORIZONTAL = true;
-        private Point2D p;
-        private boolean orientation;
+        public static final boolean VERTICAL = false;
+        public static final boolean HORIZONTAL = true;
+        public Node left;
+        public Node right;
+        public Point2D p;
+        public boolean orientation;
+        public int size;
 
         public Node(Point2D p) {
             if (p == null) throw new IllegalArgumentException();
             this.p = p;
             orientation = VERTICAL;
-        }
-
-        public Point2D getPoint() {
-            return p;
+            left = null;
+            right = null;
+            size = 1;
         }
 
         public boolean isVertical() {
@@ -40,55 +41,92 @@ public class KdTree {
         }
 
         public int compareTo(Node that) {
-            // orientation is vertical, compare x
+            // if orientation of that is vertical, compare x
             if (that.isVertical()) {
                 this.orientation = HORIZONTAL;
                 return Double.compare(this.p.x(), that.p.x());
             }
-            // orientation is horizontal, compare y
+            // if orientation of that is horizontal, compare y
             this.orientation = VERTICAL;
             return Double.compare(this.p.y(), that.p.y());
         }
     }
 
-    // is the set empty?
+    // is the tree empty?
     public boolean isEmpty() {
-        return set.isEmpty();
+        return root == null;
     }
 
-    // number of points in the set
+    // number of points in the tree
     public int size() {
-        return set.size();
+        if (isEmpty()) return 0;
+        return root.size;
     }
 
-    // add the point to the set (if it is not already in the set)
+    // add the point to the tree (if it is not already in the tree)
     public void insert(Point2D p) {
-        set.add(new Node(p));
+        Node newNode = new Node(p);
+        if (root == null) {
+            root = newNode;
+            return;
+        }
+        Node current = root;
+        while (newNode.p.compareTo(current.p) != 0) {
+            current.size++;
+            if (newNode.compareTo(current) < 0) current = current.left;
+            current = current.right;
+            if (current == null) {
+                current = newNode;
+                return;
+            }
+        }
     }
 
-    // does the set contain point p?
+    // does the tree contain point p?
     public boolean contains(Point2D p)  {
-        return set.contains(new Node(p));
+        if (isEmpty()) return false;
+        Node searchNode = new Node(p);
+        Node current = root;
+        while (current != null) {
+            if (current.p.compareTo(p) == 0) return true;
+            if (searchNode.compareTo(current) < 0) current = current.left;
+            current = current.right;
+        }
+        return false;
     }
 
     // draw all points to standard draw
     public void draw()  {
-        for (Node node : set) {
-            StdDraw.setPenColor(StdDraw.BLACK);
-            StdDraw.setPenRadius(0.01);
-            node.getPoint().draw();
+        if (isEmpty()) return;
 
-            if (node.isVertical()) {
-                StdDraw.setPenColor(StdDraw.RED);
-                StdDraw.setPenRadius(0.005);
-                StdDraw.line(node.getPoint().x(), 0, node.getPoint().x(), 1);
-            }
-            else {
-                StdDraw.setPenColor(StdDraw.BLUE);
-                StdDraw.setPenRadius(0.005);
-                StdDraw.line(0, node.getPoint().y(), 1, node.getPoint().y());
-            }
+        // draw root node point & line
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.setPenRadius(0.01);
+        root.p.draw();
+        StdDraw.setPenColor(StdDraw.RED);
+        StdDraw.setPenRadius(0.005);
+        StdDraw.line(root.p.x(), 0, root.p.x(), 1);
+
+        Node current = root;
+        while (current != null) {
+            drawSubtree(current);
         }
+
+
+        if (node.isVertical()) {
+            StdDraw.setPenColor(StdDraw.RED);
+            StdDraw.setPenRadius(0.005);
+            StdDraw.line(node.getPoint().x(), 0, node.getPoint().x(), 1);
+        }
+        else {
+            StdDraw.setPenColor(StdDraw.BLUE);
+            StdDraw.setPenRadius(0.005);
+            StdDraw.line(0, node.getPoint().y(), 1, node.getPoint().y());
+        }
+    }
+
+    private void drawSubtree(Node parent) {
+
     }
 
     // all points that are inside the rectangle (or on the boundary)
@@ -97,7 +135,7 @@ public class KdTree {
         return null;
     }
 
-    // a nearest neighbor in the set to point p; null if the set is empty
+    // a nearest neighbor in the tree to point p; null if the tree is empty
     public Point2D nearest(Point2D p) {
         // TODO
         return null;
@@ -112,6 +150,5 @@ public class KdTree {
 
         // Test draw()
         // kdTree.draw();
-
     }
 }
