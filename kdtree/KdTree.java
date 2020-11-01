016cb1b7db82df60693c1f6b37a5252e16c52348
Blend Hamiti <b.hamiti.9@gmail.com>
2020-11-01 15:34:31 +0100

    implemented draw method

--- kdtree/KdTree.java
+++ kdtree/KdTree.java
@@ -10,8 +10,6 @@ import edu.princeton.cs.algs4.RectHV;
 import edu.princeton.cs.algs4.StdDraw;
 
 public class KdTree {
-    private static final boolean LEFT = false;
-    private static final boolean RIGHT = true;
     private Node root;
 
     // construct an empty set of points
@@ -22,8 +20,11 @@ public class KdTree {
     private class Node implements Comparable<Node> {
         public static final boolean VERTICAL = false;
         public static final boolean HORIZONTAL = true;
+        public Node parent;
         public Node left;
         public Node right;
+        public double leftLimit;
+        public double rightLimit;
         public Point2D p;
         public boolean orientation;
         public int size;
@@ -32,9 +33,12 @@ public class KdTree {
             if (p == null) throw new IllegalArgumentException();
             this.p = p;
             orientation = VERTICAL;
+            size = 1;
+            parent = null;
             left = null;
             right = null;
-            size = 1;
+            leftLimit = 0;
+            rightLimit = 1;
         }
 
         public boolean isVertical() {
@@ -42,14 +46,10 @@ public class KdTree {
         }
 
         public int compareTo(Node that) {
-            // if orientation of that is vertical, compare x
-            if (that.isVertical()) {
-                this.orientation = HORIZONTAL;
+            if (that.isVertical())
                 return Double.compare(this.p.x(), that.p.x());
-            }
-            // if orientation of that is horizontal, compare y
-            this.orientation = VERTICAL;
-            return Double.compare(this.p.y(), that.p.y());
+            else
+                return Double.compare(this.p.y(), that.p.y());
         }
     }
 
@@ -67,30 +67,30 @@ public class KdTree {
     // add the point to the tree (if it is not already in the tree)
     public void insert(Point2D p) {
         Node newNode = new Node(p);
-        if (root == null) {
-            root = newNode;
-            return;
-        }
+        if (root == null) root = newNode;
         Node current = root;
         while (newNode.p.compareTo(current.p) != 0) {
             current.size++;
             if (newNode.compareTo(current) < 0) {
-                if (current.left != null) {
-                    current = current.left;
-                    continue;
+                if (current.left == null) {
+                    newNode.parent = current;
+                    newNode.orientation = !newNode.parent.orientation;
+                    newNode.leftLimit = newNode.parent.parent == null ? 0 : newNode.parent.parent.leftLimit;
+                    newNode.rightLimit = newNode.parent.isVertical() ? newNode.parent.p.x() : newNode.parent.p.y();
+                    current.left = newNode;
                 }
-                current.left = newNode;
-                return;
+                current = current.left;
             }
             else {
-                if (current.right != null) {
-                    current = current.right;
-                    continue;
+                if (current.right == null) {
+                    newNode.parent = current;
+                    newNode.orientation = !newNode.parent.orientation;
+                    newNode.leftLimit = newNode.parent.isVertical() ? newNode.parent.p.x() : newNode.parent.p.y();
+                    newNode.rightLimit = newNode.parent.parent == null ? 1 : newNode.parent.parent.rightLimit;
+                    current.right = newNode;
                 }
-                current.right = newNode;
-                return;
+                current = current.right;
             }
-
         }
     }
 
@@ -102,7 +102,7 @@ public class KdTree {
         while (current != null) {
             if (current.p.compareTo(p) == 0) return true;
             if (searchNode.compareTo(current) < 0) current = current.left;
-            current = current.right;
+            else                                   current = current.right;
         }
         return false;
     }
@@ -110,57 +110,41 @@ public class KdTree {
     // draw all points to standard draw
     public void draw()  {
         if (isEmpty()) return;
-        drawSubtrees(null, root);
+        drawSubtrees(root);
     }
 
-    private void drawSubtrees(Node parent, Node child) {
-        if (child.left != null) drawSubtrees(child, child.left);
-        if (child.right != null) drawSubtrees(child, child.right);
-        drawNode(parent, child);
+    private void drawSubtrees(Node node) {
+        if (node.left != null) drawSubtrees(node.left);
+        if (node.right != null) drawSubtrees(node.right);
+        drawNode(node);
     }
 
-    private void drawNode(Node parent, Node child) {
-        drawLine(parent, child);
-        drawPoint(child);
+    private void drawNode(Node node) {
+        drawLine(node);
+        drawPoint(node);
     }
 
     private void drawPoint(Node node) {
         StdDraw.setPenColor(StdDraw.BLACK);
         StdDraw.setPenRadius(0.015);
         node.p.draw();
+
+        // // Draw the coordinate point text
+        // StdDraw.setPenColor(StdDraw.MAGENTA);
+        // StdDraw.setPenRadius(0.005);
+        // StdDraw.text(node.p.x(), node.p.y(), "(" + node.p.x() + ", " + node.p.y() + ")");
     }
 
-    private void drawLine(Node parent, Node child) {
-        StdDraw.setPenRadius(0.005);
-
-        // draw root node
-        if (parent == null) {
+    private void drawLine(Node node) {
+        if (node.isVertical()) {
+            StdDraw.setPenRadius(0.005);
             StdDraw.setPenColor(StdDraw.RED);
-            StdDraw.line(child.p.x(), 0, child.p.x(), 1);
+            StdDraw.line(node.p.x(), node.leftLimit, node.p.x(), node.rightLimit);
         }
-
-        // draw left child
-        else if (child == parent.left) {
-            if (child.isVertical()) {
-                StdDraw.setPenColor(StdDraw.RED);
-                StdDraw.line(child.p.x(), 0, child.p.x(), parent.p.y());
-            }
-            else {
-                StdDraw.setPenColor(StdDraw.BLUE);
-                StdDraw.line(0, child.p.y(), parent.p.x(), child.p.y());
-            }
-        }
-
-        // draw right child
         else {
-            if (child.isVertical()) {
-                StdDraw.setPenColor(StdDraw.RED);
-                StdDraw.line(child.p.x(), parent.p.y(), child.p.x(), 1);
-            }
-            else {
-                StdDraw.setPenColor(StdDraw.BLUE);
-                StdDraw.line(parent.p.x(), child.p.y(), 1, child.p.y());
-            }
+            StdDraw.setPenRadius(0.005);
+            StdDraw.setPenColor(StdDraw.BLUE);
+            StdDraw.line(node.leftLimit, node.p.y(), node.rightLimit, node.p.y());
         }
     }
 
@@ -178,7 +162,7 @@ public class KdTree {
 
     // unit testing of the methods (optional)
     public static void main(String[] args) {
-        In in = new In("input5b.txt");
+        In in = new In("input10.txt");
         KdTree kdTree = new KdTree();
         while (!in.isEmpty())
             kdTree.insert(new Point2D(in.readDouble(), in.readDouble()));
